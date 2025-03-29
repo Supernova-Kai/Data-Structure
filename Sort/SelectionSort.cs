4ffcd698e71576e8d3afbcf8dbbf751f8bdad9f6
dikai <zhoudikai@qq.com>
2025-03-29 17:12:37 +0800

    feat:插入排序、修改注释

--- Sort/SelectionSort.cs
+++ Sort/SelectionSort.cs
@@ -1,32 +1,34 @@
 ﻿/*
  * 选择排序
- * 
- * 原理：
- * 1、在未排序部分选出最大或最小的元素（取决升序还是降序）
- * 2、将其与未排序部分的第一或者最后一个（取决升序还是降序）元素交换
- * 3、重复以上步骤，直到完成
+ * 时间复杂度：O(n^2)
+ * 相比于冒泡排序，内层循环只有一次交换的步骤，性能相比于冒泡排序更高
+ * 空间复杂度：不开辟新空间，为O(1)
  */
 
 public partial class Sort
 {
-    /*
-     * 这里我们以升序为例，每一次找出未排序部分的最小元素，并将它放到最前面
-     */
     public static void SelectionSort(int[] array)
     {
-        for (int i = 0; i < array.Length - 1; i++)
+        // 外层for循环，控制轮数，这里的i理解成，索引i需要找到一个正确的值，那么我们需要在未排序部分找到最小值，并将它与索引i交换
+        // 假设我们有个数组，长度为5，其实只需要4轮便可以排完序列，因为选择排序最终是交换两个元素的位置，如果第4轮出现最后两个元素错误
+        // 交换之后它们其实都已经处于正确的位置了，不需要在走一轮
+        // 所以外层循环的轮数是长度减一
+        for (int i = 0; i < array.Length - 1; i++) // i理解为“索引i需要一个正确的值”
         {
-            int minIndex = i; //因为要与未排序部分最前面的值交换，暂且认为未排序部分的第一个元素是最小的
-            for (int j = i + 1; j < array.Length; j++)
+            int minIndex = i; //先假定索引i是最小的
+
+            //下面的循环用于在未排序部分中找到最小的元素
+            for (int j = i + 1; j < array.Length; j++) //未排序部分就是索引i之后的部分
             {
                 if (array[j] < array[minIndex])
                 {
-                    minIndex = j; //找到更小的就更新索引
+                    minIndex = j; //找到更小的元素就更新索引
                 }
             }
 
             if (minIndex != i)
             {
+                //交换索引i与最小值的索引，那么索引i就是正确的值了
                 int temp = array[minIndex];
                 array[minIndex] = array[i];
                 array[i] = temp;
