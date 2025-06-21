e562cd4879ef9f367ca6fdd2e0a9839f57f3a4a0
Supernova-Kai <zhoudikai@qq.com>
2025-06-21 17:53:40 +0800

    feat - 冒泡排序

---
+++ Data-Structure/算法/排序/冒泡排序/冒泡排序.cs
@@ -0,0 +1,18 @@
+﻿public partial class Sort 
+{
+    public static void BubbleSort(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++) //集合长度为n，只需要n-1次就可完成排序
+        {
+            for (int j = 0; j < array.Length - 1 - i; j++) //从前到尾进行冒泡，未排序部分最大的数便会不断冒泡到末尾
+            {
+                if (array[j + 1] < array[j])
+                {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ Data-Structure/算法/排序/冒泡排序/冒泡排序优化(提前中断).cs
@@ -0,0 +1,25 @@
+﻿public partial class Sort
+{
+    public static void BubbleSort2(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            bool swapped = false;  // 标记本轮是否发生过交换
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                if (array[j + 1] < array[j])
+                {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    swapped = true;  // 发生交换，标记为 true
+                }
+            }
+
+            if (!swapped)  // 如果本轮没有交换，说明数组已经有序，提前结束
+            {
+                break;
+            }
+        }
+    }
+}
